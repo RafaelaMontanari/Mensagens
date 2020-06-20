f3e8372babae827183f7b2c00b84a1ed892a4b8a
RafaelaMontanari <rafaela_silva_cunha@hotmail.com>
2020-06-19 21:18:00 -0300

    add app mensagens

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Rafaela on 19/06/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    @IBOutlet weak var lbMessage: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func changeColor(_ sender: UIButton) {
+        
+    }
+
+}
